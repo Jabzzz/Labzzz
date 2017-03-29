3a761e6a60c265bae5129f3493e2a484de0a47d7
DiabolicGoblin <samvell@web.de>
2017-03-29 21:19:05 +0200

    Add LibGDX Project
    Added LibGDX Projects for Android, IOS and Desktop.

---
+++ android/src/com/jabzzz/labzzz/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.jabzzz.labzzz;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.jabzzz.labzzz.MainGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MainGame(), config);
+	}
+}
---
+++ desktop/src/com/jabzzz/labzzz/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.jabzzz.labzzz.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.jabzzz.labzzz.MainGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainGame(), config);
+	}
+}
