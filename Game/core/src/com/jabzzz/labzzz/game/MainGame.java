df154a22b2b962476a48b910a5284eec077fa894
DiabolicGoblin <samvell@web.de>
2017-04-01 20:03:54 +0200

    Added Background to Menu
    Added an Background-Image
    Modified MainGame for MenuState

--- Game/core/src/com/jabzzz/labzzz/game/MainGame.java
+++ Game/core/src/com/jabzzz/labzzz/game/MainGame.java
@@ -5,6 +5,10 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.jabzzz.labzzz.states.MenuState;
+import com.jabzzz.labzzz.states.State;
+
+import java.util.Stack;
 
 public class MainGame extends ApplicationAdapter
 {
@@ -13,6 +17,18 @@ public class MainGame extends ApplicationAdapter
 	public static final String TITLE = "THE Game";
 	public static final int WIDTH = 500;
 	public static final int HEIGHT = 850;
+
+
+
+
+	private MenuState theMenuState = null;
+	private UpdateThread theUpdateThread = null;
+
+	private Stack<State> theStateStack = new Stack();
+
+
+	private int frames = 0;
+	private long lastOut = 0;
 
 
 
@@ -26,18 +42,20 @@ public class MainGame extends ApplicationAdapter
 		img = new Texture("badlogic.jpg");
 
 
+		//init printFPS
+		lastOut = System.currentTimeMillis();
+
+
+		//init MenuState
+		theStateStack.add(theMenuState = new MenuState());
+
+
+		theUpdateThread = new UpdateThread();
 
 	}
 
-	private int frames = 0;
-	private long lastOut = 0;
-
-	@Override
-	public void render () 
+	private void printFPS()
 	{
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
 		frames++;
 
 		if(System.currentTimeMillis() - lastOut > 1000)
@@ -46,11 +64,20 @@ public class MainGame extends ApplicationAdapter
 			frames = 0;
 			lastOut = System.currentTimeMillis();
 		}
+	}
 
 
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+	@Override
+	public void render () 
+	{
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+
+		printFPS();
+
+		theStateStack.peek().render();
+
 	}
 
 
@@ -64,7 +91,6 @@ public class MainGame extends ApplicationAdapter
 	@Override
 	public void dispose ()
 	{
-		batch.dispose();
-		img.dispose();
+		theStateStack.peek().dispose();
 	}
 }
--- Game/core/src/com/jabzzz/labzzz/states/MenuState.java
+++ Game/core/src/com/jabzzz/labzzz/states/MenuState.java
@@ -1,6 +1,7 @@
 package com.jabzzz.labzzz.states;
 
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.Texture;
+import com.jabzzz.labzzz.game.MainGame;
 
 /**
  * Created by Useless on 01.04.2017.
@@ -9,14 +10,40 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class MenuState extends State
 {
 
+    Texture backgroundTexture = null;
+
+
+    public MenuState()
+    {
+        backgroundTexture = new Texture("menu_background.jpg");
+
+
+    }
+
+
+
 
     @Override
-    public void render(SpriteBatch theBatch) {
+    public void render()
+    {
+        theBatch.begin();
+
+
+        theBatch.draw(backgroundTexture, 0, 0, MainGame.WIDTH, MainGame.HEIGHT);
+
+
+        theBatch.end();
+    }
+
+    @Override
+    public void update()
+    {
 
     }
 
     @Override
-    public void update() {
-
+    public void dispose()
+    {
+        theBatch.dispose();
     }
 }
