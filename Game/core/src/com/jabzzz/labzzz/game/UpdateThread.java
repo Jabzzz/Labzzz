139bd7fd3cd9e20d46036b710971b1eaa471f606
Jooshua <joshua.ammermann@online.de>
2017-04-01 19:38:28 +0200

    UpdateThread
    worked on UpdateThread

--- Game/core/src/com/jabzzz/labzzz/game/UpdateThread.java
+++ Game/core/src/com/jabzzz/labzzz/game/UpdateThread.java
@@ -4,7 +4,29 @@ package com.jabzzz.labzzz.game;
  * Created by Useless on 01.04.2017.
  */
 
-public class UpdateThread extends Thread{
+public class UpdateThread extends Thread
+{
+    private boolean running = true;
 
-    
+
+    @Override
+    public void run()
+    {
+        while(running){
+
+        }
+
+
+
+    }
+
+    public void setRunning(boolean running)
+    {
+        this.running = running;
+    }
+
+    public boolean getRunning ()
+    {
+        return this.running;
+    }
 }
