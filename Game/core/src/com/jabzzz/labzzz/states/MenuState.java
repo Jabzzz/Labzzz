2a16c6eaa7799f4ceb711d9d1f98b9b326225a7a
DiabolicGoblin <samvell@web.de>
2017-04-01 20:51:28 +0200

    Deleted old files & modified MenuState
    Added a Textfont to the Menustate

--- Game/core/src/com/jabzzz/labzzz/states/MenuState.java
+++ Game/core/src/com/jabzzz/labzzz/states/MenuState.java
@@ -1,6 +1,9 @@
 package com.jabzzz.labzzz.states;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.jabzzz.labzzz.game.MainGame;
 
 /**
@@ -10,13 +13,16 @@ import com.jabzzz.labzzz.game.MainGame;
 public class MenuState extends State
 {
 
-    Texture backgroundTexture = null;
+    private Texture backgroundTexture = null;
+    private BitmapFont textFont = null;
 
 
     public MenuState()
     {
         backgroundTexture = new Texture("menu_background.jpg");
 
+        textFont = new BitmapFont(Gdx.files.internal("textfont.fnt"), false);
+        textFont.setColor(Color.WHITE);
 
     }
 
@@ -30,6 +36,8 @@ public class MenuState extends State
 
 
         theBatch.draw(backgroundTexture, 0, 0, MainGame.WIDTH, MainGame.HEIGHT);
+
+        textFont.draw(theBatch, "The Menu", ((MainGame.WIDTH - 150) / 2) + (int) (20 * (Math.cos(System.currentTimeMillis() * 0.01))), (int) ((MainGame.HEIGHT - 200) + 50 * Math.sin(System.currentTimeMillis() * 0.001)));
 
 
         theBatch.end();
